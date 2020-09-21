abcd2bd005fe12fa685a303155cf75243d14c89b
hana ware <hana.ware@live.co.uk>
2020-09-21 10:43:00 -0700

    l

--- src/containers/Dealer.js
+++ src/containers/Dealer.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useFormFields } from "../libs/hooksLib";
-import {
-  FormGroup,
-  FormControl,
-  ControlLabel,
-} from "react-bootstrap";
+import {FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
+import { onError } from "../libs/errorLib";
+import config from "../config";
+import "./NewNote.css";
+import { s3Upload } from "../libs/awsLibs"
 
 export default function AddDealer() {
     const [fields, handleFieldChange] = useFormFields({
@@ -20,9 +20,31 @@ export default function AddDealer() {
     });
     const [isLoading, setIsLoading] = useState(false);
     const [content, setContent] = useState("");
+    const file = useRef(null);
 
     async function handleSubmit(event) {
+      event.preventDefault();
 
+      if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+        // alert(
+        //   `Please pick a file smaller than ${
+        //   config.MAX_ATTACHMENT_SIZE / 1000000
+        //   } MB.`
+        // );
+        return;
+      }
+
+      setIsLoading(true);
+
+      try {
+        const attachment = file.current ? await s3Upload(file.current) : null;
+
+        // await createNote({ content, attachment });
+        // history.push("/");
+      } catch (e) {
+        onError(e);
+        setIsLoading(false);
+      }
     }
 
     function validateForm() {
